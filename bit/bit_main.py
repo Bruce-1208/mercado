95a62fa70df735fa78a646cac655f72008d5e24b
zzw-pc <1013459852@qq.com>
2026-04-22 22:48:10 +0800

    1

--- bit/bit_main.py
+++ bit/bit_main.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     scheduler = BlockingScheduler()
     scheduler.add_job(get_reputation_info_all, 'cron', hour=6, minute=00)
     scheduler.add_job(get_reputation_info_all, 'cron', hour=10, minute=00)
-    scheduler.add_job(download_summary, 'cron', hour=00, minute=00)
+    scheduler.add_job(download_summary, 'cron', hour=14, minute=00)
 
     scheduler.start()
 
